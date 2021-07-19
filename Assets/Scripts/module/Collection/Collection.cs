8771bb6824ae86b4cdce0363a9a2fd7a6f6769ad
uni-horihori <71215499+uni-horihori@users.noreply.github.com>
2021-07-19 22:57:58 +0800

    add collection system

---
+++ Assets/Scripts/module/Collection/Collection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class Collection
+{
+    private static HashMap<int, String> NameMap = new HashMap<int, string>();
+    private static HashMap<int, String> DescriptMap = new HashMap<int, string>();
+    
+    private int ID;
+    private String Name;
+    private String Description;
+
+    public Collection(int id)
+    {
+        this.ID = id;
+        this.Name = NameMap.Get(id);
+        this.Description = DescriptMap.Get(id);
+    }
+
+    public static void Init()
+    {
+        InitNameMap();
+        InitDescriptMap();
+    }
+
+    static void InitNameMap()
+    {
+        NameMap.Add(0,"balloon");
+        
+    }
+
+    static void InitDescriptMap()
+    {
+        DescriptMap.Add(0,"balloon");
+    }
+
+    public String GetName()
+    {
+        return this.Name;
+    }
+
+    public String GetDescription()
+    {
+        return this.Description;
+    }
+
+}
---
+++ Assets/Scripts/module/Collection/CollectionList.cs
@@ -0,0 +1,16 @@
+﻿using Fungus;
+using UnityEngine.UIElements;
+
+public class CollectionList
+{
+    public static int COLLECTIONCNT = 30;
+    private static Collection[] AllCollections;
+    private static int[] Unlocked;
+
+    public static void Init()
+    {
+        AllCollections = new Collection[COLLECTIONCNT];
+        Unlocked = new int[COLLECTIONCNT];
+    }
+
+}
