5dcb4e4960e0d838ec880288fc06bf444a73d49a
uni-horihori <71215499+uni-horihori@users.noreply.github.com>
2021-07-20 17:18:23 +0800

    implement collectionList

--- Assets/Scripts/module/Collection/GridBehavior.cs
+++ Assets/Scripts/module/Collection/GridBehavior.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridBehavior : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    private int ID;
+    private Boolean Locked = true;
+
+    private GameObject LockIcon;
+    private GameObject CollectionIcon;
+
+    private static int MAXCOLLCNT = 4;
+
+   
     void Start()
     {
+        gameObject.GetComponent<Button>().onClick.AddListener(ShowCollectionDetails);
         
     }
 
@@ -16,8 +29,43 @@ public class GridBehavior : MonoBehaviour
         
     }
 
-    public void Init(int id)
+    public void Init(int id,Boolean isLocked)
     {
+        if (id >= MAXCOLLCNT)
+        {
+            gameObject.transform.FindChild("lock").gameObject.SetActive(false);
+            return;
+        }
+
+        this.ID = id;
+        this.Locked = isLocked;
+        String CollectionName = "coll" + (this.ID+1);
+        LockIcon = gameObject.transform.FindChild("lock").gameObject;
+        CollectionIcon = gameObject.transform.Find(CollectionName).gameObject;
+        ChangeIconVisibility(); 
         
+        
+    }
+
+    public void SetIconVisibility(Boolean locked)
+    {
+        this.Locked = locked;
+        ChangeIconVisibility();
+    }
+
+    private void ChangeIconVisibility()
+    {
+        LockIcon.SetActive(Locked);
+        CollectionIcon.SetActive(!Locked);
+    }
+
+    private void ShowCollectionDetails()
+    {
+        if (this.Locked)
+        {
+            
+        }
+
+        Debug.Log(this.ID);
     }
 }
